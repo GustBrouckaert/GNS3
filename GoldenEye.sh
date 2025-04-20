0158c7be277537f0c2ae7d4a6128c951f8010991
GustBrouckaert <gust.brouckaert@gmail.com>
2025-04-20 18:50:33 +0300

    hahahh'

--- GoldenEye.sh
+++ GoldenEye.sh
@@ -8,7 +8,7 @@ echo "Starting tcpdump capture: $PCAP_FILE"
 sudo tcpdump -i "$INTERFACE" dst 205.174.165.68 -w "$PCAP_FILE" &
 TCPDUMP_PID=$!
 
-python3 goldeneye.py "$TARGET" -w 40 -s 100
+python3 GoldenEye/goldeneye.py "$TARGET" -w 40 -s 100
 
 kill "$TCPDUMP_PID"
 echo "DDoS simulation complete. PCAP saved to $PCAP_FILE"
